2acbb121f72fb27f3f37262c7a08bd7c8a73b433
Ahmed <ahmaat19@gmail.com>
2023-05-09 12:10:04 +0300

    adding sales order with formatted request data

--- pages/api/mobile/orders/index.ts
+++ pages/api/mobile/orders/index.ts
@@ -2,41 +2,64 @@ import nc from 'next-connect'
 import db from '../../../../config/db'
 import { isAuth } from '../../../../utils/auth'
 import axios from 'axios'
-import Order from '../../../../models/Order'
 
 const handler = nc()
 
-handler.use(isAuth)
+// handler.use(isAuth)
+
+const config = () => {
+  return {
+    headers: {
+      'x-db-key': 3020,
+    },
+  }
+}
 
 handler.post(
   async (req: NextApiRequestExtended, res: NextApiResponseExtended) => {
     await db()
+
     try {
-      const { orderItems, address, deliveryAddress } = req.body
+      let { branch } = req.query
+      branch = branch.split(' ')[0]
+      const deliveryAddress = req.body.deliveryAddress
+      req.body = {
+        products: req.body.cartItems?.map((item: any) => ({
+          _id: item._id,
+          warehouse: item.warehouse,
+          product: {
+            _id: item.product._id,
+            sku: item.product.sku,
+            name: item.product.name,
+          },
+          cost: item.cost,
+          price: item.price,
+          quantity: item.quantity,
+          color: item.color,
+          size: item.size,
+          weight: item.weight,
+          name: item.product.name,
+          currentQuantity: item.newQuantity,
+          discount: item.discount || 0,
+        })),
+        warehouse: req.body.cartItems[0].warehouse,
+        extra: false,
+        customer: req.body.customer,
+        payInFull: true,
+        account: req.body.account,
+        reference: '',
+        description: '',
+      }
 
-      const { data } = await axios.post(`${process.env.API_URL}/sales`, {
-        orderItems,
-      })
+      const url = `${process.env.API_URL}/mobile/orders?branch=${branch}`
 
-      if (!data) return res.status(400).json({ error: 'Order not created' })
+      const { data } = await axios.post(url, req.body, config())
 
-      const objectOrder = await Order.create({
-        orderItems,
-        user: req.user._id,
-        shippingAddress: {
-          address,
-          deliveryAddress,
-        },
-      })
-
-      if (!objectOrder)
-        return res
-          .status(400)
-          .json({ error: 'Order process has not been completed' })
-
-      res.status(201).json(objectOrder)
+      res.status(200).json(data)
     } catch (error: any) {
-      res.status(500).json({ error: error.message })
+      res
+        .status(500)
+        .json({ error: error?.response?.data?.error || error?.message })
     }
   }
 )
