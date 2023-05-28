b1c470dc433e8cb49f4c0b318a868dff7f52a8ba
Ahmed <ahmaat19@gmail.com>
2023-05-28 16:39:48 +0300

    fixing type error in internet recharge

--- utils/InternetRecharge.ts
+++ utils/InternetRecharge.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
 
 const {
   INTERNET_RECHARGE_URL,
-  INTERNET_RECHARGE_BUSINESS_KEY_MG,
-  INTERNET_RECHARGE_USERNAME_MG,
-  INTERNET_RECHARGE_PASSWORD_MG,
+  INTERNET_RECHARGE_BUSINESS_KEY_MGQ,
+  INTERNET_RECHARGE_USERNAME_MGQ,
+  INTERNET_RECHARGE_PASSWORD_MGQ,
 } = process.env
 
 export const rechargeData = async ({
@@ -19,16 +20,18 @@ export const rechargeData = async ({
   const { data } = await axios.post(
     `${INTERNET_RECHARGE_URL}`,
     {
-      username: INTERNET_RECHARGE_USERNAME_MG,
-      password: INTERNET_RECHARGE_PASSWORD_MG,
+      username: INTERNET_RECHARGE_USERNAME_MGQ,
+      password: INTERNET_RECHARGE_PASSWORD_MGQ,
       amount,
       sender,
       recharge: receiver,
+      referenceId: uuidv4(),
+      transactionId: uuidv4(),
     },
     {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'X-API-KEY': INTERNET_RECHARGE_BUSINESS_KEY_MG,
+        'X-API-KEY': INTERNET_RECHARGE_BUSINESS_KEY_MGQ,
       },
     }
   )
