ba7c1834a4c76a875db4b6b8453d0bf31c50dd5c
Ahmed <ahmaat19@gmail.com>
2023-05-28 16:17:39 +0300

    fixing type error in internet recharge

--- utils/InternetRecharge.ts
+++ utils/InternetRecharge.ts
@@ -23,7 +23,7 @@ export const rechargeData = async ({
       password: INTERNET_RECHARGE_PASSWORD_MG,
       amount,
       sender,
-      recharges: receiver,
+      recharge: receiver,
     },
     {
       headers: {
